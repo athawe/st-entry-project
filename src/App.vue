d5c38145f418f877312f635f8755d6a053108082
Aaron Thawe <aaron.thawe@gmail.com>
2018-08-26 10:55:14 -0400

    Working commit
    The project works currently, though the styling certainly leaves something to be desired.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <v-content>
+      <HelloWorld></HelloWorld>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4>
+          <v-text-field
+            hint="City,Country Code"
+            label="Please enter your city here"
+            single-line
+            outline
+            v-model="cityRequest"
+          ></v-text-field>
+        </v-flex>
+        <v-flex xs12 sm6 md4>
+          <v-btn @click="requestCity">Request Weather Data</v-btn>
+        </v-flex>
+        <v-flex xs12 sm6 md4>
+          <v-text-field
+            v-model="outputData"
+            auto-grow
+            textarea
+          ></v-text-field>
+        </v-flex>
+</v-layout>
+    </v-content>
+
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
 </template>
 
+<style>
+  textarea {
+    width: 100%;
+  }
+  span {
+    padding-left: 10px;
+  }
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+
+  },
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+      first: 'John',
+      last: 'Doe',
+      cityRequest: '',
+      outputData: '',
+
+    }
+  },
+  methods: {
+    requestCity(){
+      let inputString = 'http://api.openweathermap.org/data/2.5/weather?q=' + this.cityRequest +'&appid=ab45b5b8de7f40499e679726a1e39407'
+      let request = this.$axios.get(inputString).then((response) => {
+        this.outputData = JSON.stringify(response);
+        return response;
+      });
+      return request;
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import './plugins/axios'
+import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from '../node_modules/vuetify';
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
   render: h => h(App)
